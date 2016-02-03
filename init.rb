275389c33f4af9d2c3b3eb742cf7e94bf09b9667
Ryo SUETSUGU <suetsugu.r@gmail.com>
2016-02-03 22:09:06 +0900

    init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_emoji_filter do
+  name 'Redmine Emoji Filter plugin'
+  author 'suer'
+  description 'This is a plugin for Redmine to add emoji'
+  version '0.0.1'
+  url 'https://github.com/suer/redmine_emoji_filter'
+  author_url 'https://d.hatena.ne.jp/suer'
+end
