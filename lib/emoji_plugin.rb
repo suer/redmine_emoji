d4b82d5f5a2dba271ac8632d9b9df4363c6395f0
Ryo SUETSUGU <suetsugu.r@gmail.com>
2018-03-24 16:16:01 +0900

    Rails5 support

--- lib/emoji_plugin.rb
+++ lib/emoji_plugin.rb
@@ -1,23 +1,12 @@
 module EmojiPlugin
-  module Patch
-    def self.included(base)
-      base.send(:include, InstanceMethods)
-      base.class_eval do
-        unloadable
-        alias_method_chain :to_html, :emoji
-      end
-    end
-
-    module InstanceMethods
-      def to_html_with_emoji
-        html = to_html_without_emoji
-        html.gsub(/:([^:\s]+):/) do |match|
-          icon = $1
-          if Emoji.names.include?(icon)
-            %(<img src="#{ Rails.application.routes.url_helpers.url_for(:controller => :emoji, :action => :index, :name => icon, :only_path => true)}" style="width:16px" title="#{icon}" alt="#{icon}"/>)
-          else
-            match
-          end
+  module FormatterWithEmoji
+    def to_html
+      super.gsub(/:([^:\s]+):/) do |match|
+        icon = $1
+        if Emoji.names.include?(icon)
+          %(<img src="#{ Rails.application.routes.url_helpers.url_for(:controller => :emoji, :action => :index, :name => icon, :only_path => true)}" style="width:16px" title="#{icon}" alt="#{icon}"/>)
+        else
+          match
         end
       end
     end
